b6bd4823c0672dd974f0ea28cba288ba94ae6cfc
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2024-07-04 19:52:54 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      appBar: AppBar(
+        title: const Text('Dice App'),
+      ),
+      body: DiceApp(),
+    ),
+  ));
+}
+
+class DiceApp extends StatefulWidget {
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDice = 1;
+  int rightDice = 1;
+  changeDie() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/dice$leftDice.png',
+              ),
+              onPressed: changeDie,
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDice.png'),
+              onPressed: changeDie,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
